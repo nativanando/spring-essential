f5d0d46e201e938cdb81f908318ed4d4d61e9978
nativanando <nativanando@gmail.com>
2019-09-20 22:09:38 -0300

    initial commit - setting up the spring env

---
+++ README.md
@@ -0,0 +1,4 @@
+#### source code for spring essential
+
+This repo provides an initial env to set up a spring application
+
---
+++ src/main/java/br/com/springessential/SpringEssentialApplication.java
@@ -0,0 +1,14 @@
+package br.com.springessential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEssentialApplication {
+
+	public static void main(String[] args) {
+	  
+	  SpringApplication.run(SpringEssentialApplication.class, args);
+	}
+
+}
